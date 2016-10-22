222c6354d3ff42402f66e3b9f8ffe7c54b18f613
Otavio <otaviotecnano@gmail.com>
2016-10-22 15:46:21 -0200

    Criando App Admin.Sobre

--- sobre/models.py
+++ sobre/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class SobreNos(models.Model):
     titulo = models.CharField(u'Título', max_length=100)
     descricao = models.TextField(u'Descricao')
+
+    def __str__(self):
+        return self.titulo
